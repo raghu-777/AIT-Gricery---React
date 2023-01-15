f90aa926f5eb3e1c79b80aa4aa3ae637cfbaad6d
RAGHU BS <raghubsraghu058@gmail.com>
2023-01-15 19:22:46 +0530

    Add files via upload

---
+++ Backed/Products.js
@@ -0,0 +1,53 @@
+const Products = [
+    {
+        id: "1",
+        Name: "Fresh Cabbage",
+        Price: "25",
+        Image: 'https://snz04pap001files.storage.live.com/y4m9OzNat0NiZiEjkTKgp39JzvXpzpry3FuLDD8pHgoLojrcS_ZnpYFGy4HeEiuMrS8dRKCB8Ujn9RQ5GCwclGnN6J1cWxo8O2bTHj6c3mFZ4ri-fy18HGaYjehVpcqpn78Z0GSQCLwNJc85aBHT8uzy4f6ngnlJA2qV5gDhFDv22tSGRLqCojnoeIPgq7YSdGp?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "2",
+        Name: "Fresh Orange",
+        Price: "29",
+        Image: 'https://snz04pap001files.storage.live.com/y4mJuMJOnDXESHMVo3UT-rhvgC42-TAqzGV9Z-Lx7kGMXgkW0GK_9AB_Uqgm0oN29JVMrxPMloh4I2-e6M-Hv8890v2wid6xbjasGCpVhDMyAH8RhQX68xmZSLxmd07GPSxY_ND9iNPOpSuLz7EZMZfPTZuuBU_RWpiuCdSfgFb1zGGgEnCg14lzRKtrzulbzqn?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "3",
+        Name: "Fresh Onion",
+        Price: "24",
+        Image: 'https://snz04pap001files.storage.live.com/y4mxy2-QxxTuLJnNvKgNcJHf9q8WYd0XJJHHDmVvOZ9SEVaIZDrNlr1RCHKnWjrI54o49bFkrSeMeZdm5iuR4YIQiyJQUXit5efBQfQj_Bf9wPLHAs0RXKIQS10pLhjv8Es1J_MPFOUSTBaptT2sv-u6RBMehQ46eaksxhy44RMj6JvmAB0NDhVEVWABX9WfN7x?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "4",
+        Name: "Green Lemon",
+        Price: "89",
+        Image: 'https://snz04pap001files.storage.live.com/y4mOLgRIC3RK54D6vYUj9Z3T3_rLrbB1TJmIRKb9kY8CkgEEjDVMWFqsvxTzcpBKyZcv1In-LtjPjYIjNwfmmymFHl7mAmWkmCXUIkgDM7zfmYtwZosqkbRmABVMf4we4I8PDsULKYwBwrVgOIuqahIoIM5kPa5uSQbMJ7dJERW3sprTc-nAsYtTQODQoGyGCsR?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "5",
+        Name: "Fresh Potato",
+        Price: "29",
+        Image: 'https://snz04pap001files.storage.live.com/y4mlqbmpDJrwwd_aN1ISBe2q6UvlrMkZFCFZngFc7ZfoIZamowep3O7urLlPi0tEM0Nemm-MNEaTVe9pxG0WBu1mYh4KCr39Fz_chFnxQQ-xIQwXweg-KLiOJ6wz3uWbYOQZ1--JlO-2c6vwU0skWAeHMS7RUEeUrSjZ6Env7IxJ7j48vlByH7HbESdZKHlZaoQ?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "6",
+        Name: "Fresh Avocado",
+        Price: "39",
+        Image: 'https://snz04pap001files.storage.live.com/y4m7FRTN11JKjpqfNwNyGxq3Fhpyi5qsdD3ikIYq2WE4Npx5kCwNV1IyE58yv_EJSMmiv4IKdt8QqO7_WM7eZ5PTrRheVNfwvrTphSloNjzOEeQU05RaSpkIwx8TNFzAeMdN3ZYaJkkK1E1XFOrF3rieXTXjezhB-pGRr0pnbDd7s17q-lOJX2bVjml-iZtyPvU?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "7",
+        Name: "Fresh Carrot",
+        Price: "25",
+        Image: 'https://snz04pap001files.storage.live.com/y4mFl01EuSnZ3mm0uIgYoE-LzXrUUsaBs2pwDqsctJ4Es5ZBKPva8DXnvsjcYMWpLpCxqiGdi14jXozG24MguR9lZANQg0wKBTwv6biDezdqQYuTmIL3e4Uz7AnsWf1VaXfsASXgeELsVq9CjAy3gTX9FKuC16CXAr_4PNqe80-yp0oasSSuGQe2VfNVS7TmiUF?width=600&height=500&cropmode=none'
+    },
+    {
+        id: "8",
+        Name: "Fresh Meat",
+        Price: "19",
+        Image: 'https://snz04pap001files.storage.live.com/y4m0eYp2zoLxl03g57WlEYqxiElp12Ub2M83TlE8xv5WulnQ_sRIVIMvEWqHmphiGAu0NrOACbxlAdwFsLlxt0TrAPAbz7fI5hyGMe6OW2g8LqNwz4i1kWNbz6pGb4Md568ZHDA3QOqyUaybDlKIDzreZWAHOUDGIta7uDWTp5ZYEXclbfk64BeQ4bK2S_BgM-Y?width=600&height=500&cropmode=none'
+    }
+
+];
+
+module.exports = Products;
---
+++ Backed/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const products = require('./Products')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/products', (req, res) => {
+    res.send(products)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`Server running on Port ${port}`))
